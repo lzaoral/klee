f197671f8c08b8dd73b5d785a5e285b3cbf26b1e
Michael Simacek <msimacek@redhat.com>
2018-01-13 18:20:14 +0100

    Avoid direct usage of address in getAddressInfo

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2974,7 +2974,7 @@ std::string Executor::getAddressInfo(ExecutionState &state,
     const MemoryObject *mo = lower->first;
     std::string alloc_info;
     mo->getAllocInfo(alloc_info);
-    info << "object at " << mo->address
+    info << "object at " << mo->getAddressString()
          << " of size " << mo->size << "\n"
          << "\t\t" << alloc_info << "\n";
   }
@@ -2987,7 +2987,7 @@ std::string Executor::getAddressInfo(ExecutionState &state,
       const MemoryObject *mo = lower->first;
       std::string alloc_info;
       mo->getAllocInfo(alloc_info);
-      info << "object at " << mo->address 
+      info << "object at " << mo->getAddressString()
            << " of size " << mo->size << "\n"
            << "\t\t" << alloc_info << "\n";
     }

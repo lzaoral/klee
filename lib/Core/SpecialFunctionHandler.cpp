a6e93aebc8792b81e38c3345375cbec3be6fbd28
Michael Simacek <msimacek@redhat.com>
2018-02-16 00:57:30 +0100

    Remove few invalid TODOs

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -261,7 +261,6 @@ SpecialFunctionHandler::readStringAtAddress(ExecutionState &state,
   }
 
   bool res __attribute__ ((unused));
-  // TODO segment?
   assert(executor.solver->mustBeTrue(state, 
                                      EqExpr::create(address.getOffset(),
                                                     op.first->getBaseExpr()),
@@ -478,7 +477,6 @@ void SpecialFunctionHandler::handleAssume(ExecutionState &state,
                             const std::vector<Cell> &arguments) {
   assert(arguments.size()==1 && "invalid number of arguments to klee_assume");
 
-  // TODO segment
   ref<Expr> e = arguments[0].value;
   
   if (e->getWidth() != Expr::Bool)
@@ -551,7 +549,6 @@ void SpecialFunctionHandler::handleSetForking(ExecutionState &state,
                                               const std::vector<Cell> &arguments) {
   assert(arguments.size()==1 &&
          "invalid number of arguments to klee_set_forking");
-  // TODO segment
   ref<Expr> value = executor.toUnique(state, arguments[0].value);
   
   if (ConstantExpr *CE = dyn_cast<ConstantExpr>(value)) {
@@ -848,7 +845,6 @@ void SpecialFunctionHandler::handleMakeSymbolic(ExecutionState &state,
 
     // FIXME: Type coercion should be done consistently somewhere.
     bool res;
-    // TODO segment
     bool success __attribute__ ((unused)) =
       executor.solver->mustBeTrue(*s, 
                                   EqExpr::create(ZExtExpr::create(arguments[1].value,

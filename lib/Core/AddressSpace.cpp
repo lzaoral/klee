staticafi/JetKlee

Lookup the segment in AddressSpace::resolveOne

lib/Core/AddressSpace.cpp
@@ -24,10 +24,14 @@ void AddressSpace::bindObject(const MemoryObject *mo, ObjectState *os) {
   assert(os->copyOnWriteOwner==0 && "object already has owner");
   os->copyOnWriteOwner = cowKey;
   objects = objects.replace(std::make_pair(mo, os));
+  if (mo->segment != 0)
+    segmentMap = segmentMap.replace(std::make_pair(mo->segment, mo));
 }
 
 void AddressSpace::unbindObject(const MemoryObject *mo) {
   objects = objects.remove(mo);
+  if (mo->segment != 0)
+    segmentMap = segmentMap.remove(mo->segment);
 }
 
 const ObjectState *AddressSpace::findObject(const MemoryObject *mo) const {
@@ -54,21 +58,28 @@ ObjectState *AddressSpace::getWriteable(const MemoryObject *mo,
 
 bool AddressSpace::resolveConstantAddress(const KValue &pointer,
                                           ObjectPair &result) {
-  // TODO segment
-  uint64_t address = cast<ConstantExpr>(pointer.getValue())->getZExtValue();
-  MemoryObject hack(address);
+  uint64_t segment = cast<ConstantExpr>(pointer.getSegment())->getZExtValue();
 
-  if (const MemoryMap::value_type *res = objects.lookup_previous(&hack)) {
-    const MemoryObject *mo = res->first;
-    // Check if the provided address is between start and end of the object
-    // [mo->address, mo->address + mo->size) or the object is a 0-sized object.
-    if ((mo->size==0 && address==mo->address) ||
-        (address - mo->address < mo->size)) {
-      result = *res;
+  if (segment != 0) {
+    if (const SegmentMap::value_type *res = segmentMap.lookup(segment)) {
+      // TODO bounds check?
+      result = *objects.lookup(res->second);
       return true;
     }
+  } else {
+    uint64_t address = cast<ConstantExpr>(pointer.getValue())->getZExtValue();
+    MemoryObject hack(address);
+    if (const MemoryMap::value_type *res = objects.lookup_previous(&hack)) {
+      const MemoryObject *mo = res->first;
+      // Check if the provided address is between start and end of the object
+      // [mo->address, mo->address + mo->size) or the object is a 0-sized object.
+      if ((mo->size==0 && address==mo->address) ||
+          (address - mo->address < mo->size)) {
+        result = *res;
+        return true;
+      }
+    }
   }
-
   return false;
 }
 
@@ -81,6 +92,17 @@ bool AddressSpace::resolveOne(ExecutionState &state,
     success = resolveConstantAddress(pointer, result);
     return true;
   } else {
+    ref<ConstantExpr> segment = dyn_cast<ConstantExpr>(pointer.getSegment());
+    if (segment.isNull()) {
+      TimerStatIncrementer timer(stats::resolveTime);
+      if (!solver->getValue(state, pointer.getOffset(), segment))
+        return false;
+    }
+
+    if (!segment->isZero()) {
+      return resolveConstantAddress(pointer, result);
+    }
+
     TimerStatIncrementer timer(stats::resolveTime);
 
     // try cheap search, will succeed for any inbounds pointer

lzaoral/klee

lib/Core/AddressSpace.cpp
@@ -24,10 +24,14 @@ void AddressSpace::bindObject(const MemoryObject *mo, ObjectState *os) {
   assert(os->copyOnWriteOwner==0 && "object already has owner");
   os->copyOnWriteOwner = cowKey;
   objects = objects.replace(std::make_pair(mo, os));
+  if (mo->segment != 0)
+    segmentMap = segmentMap.replace(std::make_pair(mo->segment, mo));
 }
 
 void AddressSpace::unbindObject(const MemoryObject *mo) {
   objects = objects.remove(mo);
+  if (mo->segment != 0)
+    segmentMap = segmentMap.remove(mo->segment);
 }
 
 const ObjectState *AddressSpace::findObject(const MemoryObject *mo) const {
@@ -54,21 +58,28 @@ ObjectState *AddressSpace::getWriteable(const MemoryObject *mo,
 
 bool AddressSpace::resolveConstantAddress(const KValue &pointer,
                                           ObjectPair &result) const {
-  // TODO segment
-  uint64_t address = cast<ConstantExpr>(pointer.getValue())->getZExtValue();
-  MemoryObject hack(address);
+  uint64_t segment = cast<ConstantExpr>(pointer.getSegment())->getZExtValue();
 
-  if (const MemoryMap::value_type *res = objects.lookup_previous(&hack)) {
-    const MemoryObject *mo = res->first;
-    // Check if the provided address is between start and end of the object
-    // [mo->address, mo->address + mo->size) or the object is a 0-sized object.
-    if ((mo->size==0 && address==mo->address) ||
-        (address - mo->address < mo->size)) {
-      result = *res;
+  if (segment != 0) {
+    if (const SegmentMap::value_type *res = segmentMap.lookup(segment)) {
+      // TODO bounds check?
+      result = *objects.lookup(res->second);
       return true;
     }
+  } else {
+    uint64_t address = cast<ConstantExpr>(pointer.getValue())->getZExtValue();
+    MemoryObject hack(address);
+    if (const MemoryMap::value_type *res = objects.lookup_previous(&hack)) {
+      const MemoryObject *mo = res->first;
+      // Check if the provided address is between start and end of the object
+      // [mo->address, mo->address + mo->size) or the object is a 0-sized object.
+      if ((mo->size==0 && address==mo->address) ||
+          (address - mo->address < mo->size)) {
+        result = *res;
+        return true;
+      }
+    }
   }
-
   return false;
 }
 
@@ -81,6 +92,17 @@ bool AddressSpace::resolveOne(ExecutionState &state,
     success = resolveConstantAddress(pointer, result);
     return true;
   } else {
+    ref<ConstantExpr> segment = dyn_cast<ConstantExpr>(pointer.getSegment());
+    if (segment.isNull()) {
+      TimerStatIncrementer timer(stats::resolveTime);
+      if (!solver->getValue(state, pointer.getSegment(), segment))
+        return false;
+    }
+
+    if (!segment->isZero()) {
+      return resolveConstantAddress(KValue(segment, pointer.getOffset()), result);
+    }
+
     TimerStatIncrementer timer(stats::resolveTime);
 
     // try cheap search, will succeed for any inbounds pointer